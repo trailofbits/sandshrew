504531e6c5380a7ac67df912d573e9f536dfa35a
ex0dus-0x <ex0dus@codemuch.tech>
2019-03-25 20:49:18 -0400

    New tests

---
+++ tests/test_monocypher_eddsa.c
@@ -0,0 +1,27 @@
+/* test_monocypher_eddsa.c
+ *
+ *     Tests:
+ *         EdDSA signing fault with all-zero input
+ *
+ *     Descriptions:
+ *     	   Tests sandshrews ability to determine ...
+ *
+ *     Results:
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "monocypher.h"
+
+int
+main(int argc, char *argv[])
+{
+	uint8_t public_key[32];
+	uint8_t zero_key[32] = {0};
+		
+	crypto_sign_public_key(public_key, (const uint8_t *) argv[1]);
+
+	return 0;
+}
--- tests/test_openssl_md5.c
+++ tests/test_openssl_md5.c
@@ -8,29 +8,37 @@
  *          to determine an input that produces the same hash as a fixed
  *          concrete input (aka a 'Birthday Attack'), resulting in a hash
  *          collision.
+ *
+ *      Results:
+ *          Getting a viable solution (a working concrete input OR the original plaintext)
+ *          is probabilistic. Complex SMT queries during runtime remain problematic.
+ *
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/md5.h>
 
 
 /* create a SANDSHREW_* wrapper over OpenSSL's MD5 for concretization */
-unsigned char *
-SANDSHREW_MD5(char * input, unsigned char * output)
+void
+SANDSHREW_MD5(unsigned char * output, char * input)
 {
-	return MD5(input, 32, output);
+	MD5(input, 32, output);
 }
 
 
 int
 main(int argc, char *argv[])
 {
-	unsigned char * orig_result;
-	unsigned char * user_result;
+	unsigned char in[32];
+
+	unsigned char orig_result[16];
+	unsigned char user_result[16];
 	
-	orig_result = SANDSHREW_MD5("s0me_1nput_123", orig_result);
-	user_result = SANDSHREW_MD5(argv[1], user_result);
+	SANDSHREW_MD5(orig_result, "s0m3_1nput_123");
+	SANDSHREW_MD5(user_result, argv[1]);
 
 	/* if equal, we generated a hash collision! */
 	if (__strcmp_ssse3(orig_result, user_result) == 0)
